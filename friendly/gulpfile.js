115a454fd3250ee89997c074be710a568ee20474
Luke Segars <luke@theironyard.com>
2016-06-21 21:10:34 -0400

    Starter pack.

---
+++ friendly/gulpfile.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+gulp.task('default', ['html', 'css', 'js']);
+
+gulp.task('html', function () {
+    return gulp.src('./index.html')
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('./scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('./js/app.js')
+        .pipe(gulp.dest('./public/js'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./js/*.js', ['js']);
+    gulp.watch('./scss/*.scss', ['css']);
+    gulp.watch('./index.html', ['html']);
+});
